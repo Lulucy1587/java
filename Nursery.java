520b80123e17afb6faeb6b165642858be3fb0f57
Lulucy1587 <1796886394@qq.com>
2019-09-24 17:28:16 +0800

    third work

---
+++ Nursery.java
@@ -0,0 +1,11 @@
+public class Nursery{
+    public static void main(String[] args){
+        int i;
+        for(i=99;i>=0;i--){
+            System.out.println("N bottles of beer on the wall,N bottles of beer.");
+            System.out.println("Take one down.");
+            System.out.println("N-1 bottles of beer on the wall.");  
+        }
+        System.out.println("No more bottles of beer on the wall.");
+    }
+}
