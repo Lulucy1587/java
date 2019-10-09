b1a3b47ed9e0d3923b5c52bea57360294c83f4a9
Lulucy1587 <1796886394@qq.com>
2019-10-09 22:08:19 +0800

    the third class

---
+++ Complex.java
@@ -0,0 +1,17 @@
+
+public class Complex{
+
+	float p,q;
+	Complex(float a,float b,float c,float d) {
+		p=a+c;
+		q=b+d;
+	}
+	float add2() {
+		return p;
+	}
+	
+	float add3() {
+		return q;
+	}
+}
+	
