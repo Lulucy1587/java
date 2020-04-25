b287345cb8084f8a65ad1352d370d99e74029f79
Lulucy1587 <44701032+Lulucy1587@users.noreply.github.com>
2020-04-25 17:04:56 +0800

    桥接模式

--- Client.java
+++ Client.java
@@ -1,14 +1,14 @@
-package Ado;
+package Bri;
 
 public class Client {
-
 	public static void main(String[] args) {
-		MyFruit fruit1=new Apple();
-		MyFruit fruit2=new Banana();
+		Color color;
+		Pen pen;
 		
-		Adapter ada=new Adapter();
-		String str=ada.newPort(fruit1, fruit2);
-		System.out.println(str);
+		color=(Color)XMLUtilPen.getBean("color");
+		pen=(Pen)XMLUtilPen.getBean("pen");
 		
+		pen.setColor(color);
+		pen.draw("´óÏó");
 	}
 }
