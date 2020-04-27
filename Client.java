63d90e54a58075e75494c928df4db37a3202ae84
Lulucy1587 <44701032+Lulucy1587@users.noreply.github.com>
2020-04-27 15:31:14 +0800

    享元模式

--- Client.java
+++ Client.java
@@ -1,12 +1,21 @@
-package Fac;
+package Fly;
+
+import java.util.*;
 
 public class Client {
-
 	public static void main(String[] args) {
-		GeneralSwitchFacade gsf=new GeneralSwitchFacade();
-		gsf.on();
-		System.out.println("------------------------------");
-		gsf.off();
-				
+		Random rd=new Random();
+		PieceFactory pf=new PieceFactory();
+		for(int i=0;i<19;i++)
+			for(int j=0;j<19;j++)
+			{
+				APiece p;
+				if(rd.nextInt()%2==0)
+					p=pf.GetPiece("白棋");
+				else 
+					p=pf.GetPiece("黑棋");
+				p.Play(rd.nextInt(19), rd.nextInt(19));
+			}
+		System.out.println("总共棋子数为："+pf.GetPieceCount());
 	}
 }
---
+++ OnePiece.java
@@ -0,0 +1,12 @@
+package Fly;
+
+public class OnePiece extends APiece {
+
+	public OnePiece(String inkind) {
+		super(inkind);
+	}
+	public void Play(int x,int y) {
+		System.out.println(inkind+"ÂäÔÚ£¨"+x+","+y+")µÄÎ»ÖÃ");
+	}
+
+}
