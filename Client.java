3e0ebf59a627fd36aa51ecbb4dd38707a140b753
Lulucy1587 <44701032+Lulucy1587@users.noreply.github.com>
2020-04-28 17:12:22 +0800

    静态代理

--- Client.java
+++ Client.java
@@ -1,21 +1,8 @@
-package Fly;
-
-import java.util.*;
+package Pro;
 
 public class Client {
 	public static void main(String[] args) {
-		Random rd=new Random();
-		PieceFactory pf=new PieceFactory();
-		for(int i=0;i<19;i++)
-			for(int j=0;j<19;j++)
-			{
-				APiece p;
-				if(rd.nextInt()%2==0)
-					p=pf.GetPiece("白棋");
-				else 
-					p=pf.GetPiece("黑棋");
-				p.Play(rd.nextInt(19), rd.nextInt(19));
-			}
-		System.out.println("总共棋子数为："+pf.GetPieceCount());
+		CLocalPicShow cls=new CLocalPicShow();
+		cls.ShowPic("大白兔");
 	}
 }
