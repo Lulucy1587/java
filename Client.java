c496d471b93bddb0c141bea8e4ea9f600d2c077c
Lulucy1587 <44701032+Lulucy1587@users.noreply.github.com>
2020-04-29 21:22:30 +0800

    策略模式

--- Client.java
+++ Client.java
@@ -1,11 +1,12 @@
-package Tem;
+package Str;
 
 public class Client {
+
 	public static void main(String[] args) {
-		ADefiniteIntegral[] x=new ADefiniteIntegral[2];
-		x[0]=new DefIntegalXX(0,20,100);
-		x[1]=new DefIntegralLn(2.71,20,100);
-		for(int i=0;i<2;i++)
-			System.out.println(x[i].CalDefiniteIntegral());
+		Chef chef=new Chef();
+		ICutFruit cut=new MultiBlade();
+		chef.SetCutMethod(cut);
+		chef.CutFruit("Ёхвс");
+		
 	}
 }
