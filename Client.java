5ae8f27146c5dd765cec86e0125b2edcc019ca10
Lulucy1587 <44701032+Lulucy1587@users.noreply.github.com>
2020-04-29 22:08:53 +0800

    备忘录模式

--- Client.java
+++ Client.java
@@ -1,12 +1,35 @@
-package Str;
+package Mem;
 
 public class Client {
-
 	public static void main(String[] args) {
-		Chef chef=new Chef();
-		ICutFruit cut=new MultiBlade();
-		chef.SetCutMethod(cut);
-		chef.CutFruit("³È×Ó");
-		
+		OriginatorChess chess=new OriginatorChess(new MementoChess("ºÚ",2,2));
+		play(chess);
+		chess.SetX(3);
+		play(chess);
+		chess.SetY(6);
+		play(chess);
+		undo(chess,index);
+		undo(chess,index);
+		redo(chess,index);
+		redo(chess,index);
 	}
+	private static int index=0;
+	private static CaretakerChess caretaker=new CaretakerChess();
+	public static void play(OriginatorChess chess) {
+		index++;
+		caretaker.AddMem(chess.SavetoMemento());
+		chess.Show();
+	}
+	public static void undo(OriginatorChess chess,int i) {
+		System.out.println("********»ÚÆå***********");
+		index--;
+		chess.Restore(caretaker.getMem(index-1));
+		chess.Show();
+	}
+	public static void redo(OriginatorChess chess,int i) {
+		System.out.println("*******³·Ïú»ÚÆå*********");
+		chess.Restore(caretaker.getMem(index));
+		index++;
+		chess.Show();
+	} 
 }
